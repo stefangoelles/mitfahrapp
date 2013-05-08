9a17009e0b9fd0f4d3a05e6bd683098a586331c1
Lukas <lukas@lukasPC.(none)>
2013-05-08 16:59:12 +0200

    [1.02]<get string from edittext> (LG,JL)

--- MitfahrApp1/src/com/mitfahr/ActivityTrip.java
+++ MitfahrApp1/src/com/mitfahr/ActivityTrip.java
@@ -3,17 +3,29 @@ package com.mitfahr;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityTrip extends Activity {
+	
+	private String from;
+	private String to;
+	private String date;
+	private String time;
+	private Integer seats;
+	private String desc;
+	private EditText m_edit_text;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_trip);
+		
 		
 		Button button_weiter = (Button) findViewById(R.id.btn_weiter);
 		
@@ -21,9 +33,14 @@ public class ActivityTrip extends Activity {
 	    {
 	      public void onClick(View v)
 	      {
-	    	 Intent inte = new Intent(ActivityTrip.this, MainWindow.class);
-	    	 ActivityTrip.this.startActivity(inte);
-	    	 finish();
+	    	 //Intent inte = new Intent(ActivityTrip.this, ActivityCheckInput.class);
+	    	 
+	    	 m_edit_text = (EditText) findViewById(R.id.txt_trip_von);
+	    	 from = m_edit_text.getText().toString();
+	    	 
+	    	 Toast.makeText(getApplicationContext(), from, Toast.LENGTH_LONG).show();
+	    	 
+	    	 //ActivityTrip.this.startActivity(inte);
 	      }
 	    });
 	}
