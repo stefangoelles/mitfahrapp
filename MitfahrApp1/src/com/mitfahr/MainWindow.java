90e59de70efd11eecb23e7e236154de1841e5469
Lukas <lukas@lukasPC.(none)>
2013-05-22 15:53:59 +0200

    [0815][Refactor] refactored MainWindow.java (LG,TJ)

--- MitfahrApp1/src/com/mitfahr/MainWindow.java
+++ MitfahrApp1/src/com/mitfahr/MainWindow.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.view.View.OnClickListener;
 public class MainWindow extends Activity {
 
