c9786bc9ee5ea86c3928d528bb3eab378f56fd37
Thomas Joham <thomas.joham@student.tugraz.at>
2013-05-22 13:08:41 +0200

    [0.0] Created Test suite files [LG,TJ]

---
+++ MitfahrApp1-TestSuite/src/com/mitfahr/test/SomeTest.java
@@ -0,0 +1,18 @@
+package com.mitfahr.test;
+
+import junit.framework.Assert;
+import android.test.AndroidTestCase;
+
+
+public class SomeTest extends AndroidTestCase {
+
+    public void testSomething() throws Throwable {
+       Assert.assertTrue(( 1 + 1 == 2));
+    }
+	
+    public void testSomethingElse() throws Throwable {
+       Assert.assertTrue(1 + 1 == 2);
+    }
+        
+}
+
