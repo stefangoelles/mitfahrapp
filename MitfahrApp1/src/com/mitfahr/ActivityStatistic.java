87272ede4b96c3990b32b5cb148d162c5fb13d1f
Thomas Fötschl <thomas@Tom-Notebook.(none)>
2013-06-05 11:54:31 +0200

    [4.03] [design] [TF, LG] added statistic xml and java

---
+++ MitfahrApp1/src/com/mitfahr/ActivityStatistic.java
@@ -0,0 +1,50 @@
+package com.mitfahr;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class ActivityStatistic extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_statistic);
+		
+		
+		
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+	    case R.id.about:
+	    	startActivity(new Intent(this, com.mitfahr.ActivityImpressum.class));
+	   // startActivity(new Intent(this, Impressum.class));
+	    return true;
+	    case R.id.statistics:
+	    	startActivity(new Intent(this, com.mitfahr.ActivityStatistic.class));
+	    //startActivity(new Intent(this, Help.class));
+	    return true;
+	    case R.id.exit:
+	    	System.exit(0);
+	    default:
+	    return super.onOptionsItemSelected(item);
+	}
+
+	}
+
+}
