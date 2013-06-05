e265961ac3ab842f7bf5475db8aa132151f70b74
RichardBriesner <richard.briesner@student.tugraz.at>
2013-06-05 16:25:42 +0200

    [4.08] http-request in own class.3.TESTCASES_FIXED (DB,RB)

--- MitfahrApp1-TestSuite/src/com/mitfahr/test/OfferRideTest.java
+++ MitfahrApp1-TestSuite/src/com/mitfahr/test/OfferRideTest.java
@@ -25,10 +25,11 @@ public class OfferRideTest extends AndroidTestCase
 		String phone = "321654321";
 		String name = "Mohamet";
 		String email= "Ali@ali.com";
+		int seats = 1;
 		int hour = 5;
 		int minute = 5;
 		
-		Ride ride = new Ride(from, to, year, month, day, costs, aso, phone, name, email, hour, minute);
+		Ride ride = new Ride(from, to, year, month, day, costs, aso, phone, name, email, hour, minute, seats);
 		boolean result = RideModel.getInstance().insertRides(ride);
 		
 		
@@ -49,15 +50,16 @@ public class OfferRideTest extends AndroidTestCase
 		String aso = "blablabla";
 		String phone = "321654321";
 		String name = "Mohamet";
-		String email= "Ali@ali.com";
+		String email= "";
+		int seats = 1;
 		int hour = 5;
 		int minute = 5;
 		
-		Ride ride = new Ride(from, to, year, month, day, costs, aso, phone, name, email, hour, minute);
+		Ride ride = new Ride(from, to, year, month, day, costs, aso, phone, name, email, hour, minute, seats);
 		boolean result = RideModel.getInstance().insertRides(ride);
 		
 		
-		Assert.assertEquals(true, result);
+		Assert.assertEquals(false, result);
 	}
 	
 	
--- MitfahrApp1-TestSuite/src/com/mitfahr/test/RideTest.java
+++ MitfahrApp1-TestSuite/src/com/mitfahr/test/RideTest.java
@@ -17,7 +17,7 @@ import android.test.AndroidTestCase;
 
 public class RideTest extends AndroidTestCase
 {
-	private Ride test = new Ride(0, "asdf", "asdf", 2000, 1, 1, 1, "asdf", "asdf", "asdf", "asdf", 1, 1);
+	private Ride test = new Ride(0, "asdf", "asdf", 2000, 1, 1, 1, "asdf", "asdf", "asdf", "asdf", 1, 1, 1);
 	private boolean result = false; 
 	
 	public void testRideGetFrom(){
