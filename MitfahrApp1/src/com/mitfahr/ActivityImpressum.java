4ef183d1aad75ba7ade37683f3477f5aa3456448
Thomas Fötschl <thomas@Tom-Notebook.(none)>
2013-06-05 11:57:13 +0200

    [4.01][design] (TF, LG) changed layout

--- MitfahrApp1/src/com/mitfahr/ActivityImpressum.java
+++ MitfahrApp1/src/com/mitfahr/ActivityImpressum.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -22,7 +23,7 @@ public class ActivityImpressum extends Activity {
 		    {
 		      public void onClick(View v)
 		      {
-		    	 Intent inte = new Intent(ActivityImpressum.this, ActivityAGB.class);
+		    	// Intent inte = new Intent(ActivityImpressum.this, ActivityAGB.class);
 		    	  //ActivityImpressum.this.startActivity(inte);
 		    	  setContentView(R.layout.activity_agb);
 	
@@ -39,6 +40,24 @@ public class ActivityImpressum extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+	    case R.id.about:
+	    	startActivity(new Intent(this, com.mitfahr.ActivityImpressum.class));
+	   // startActivity(new Intent(this, Impressum.class));
+	    return true;
+	    case R.id.statistics:
+	    	startActivity(new Intent(this, com.mitfahr.ActivityStatistic.class));
+	    //startActivity(new Intent(this, Help.class));
+	    return true;
+	    case R.id.exit:
+	    	System.exit(0);
+	    default:
+	    return super.onOptionsItemSelected(item);
+	}
+
 	}
 
 }
